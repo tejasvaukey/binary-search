25b95810a3382c9e645f4fccbf0ac6ad38c31437
Tejasva Ukey <tejasvaukey123@gmail.com>
2026-04-30 21:24:29 +0530

    race track code, pw vdo 48

--- src/Problems.java
+++ src/Problems.java
@@ -75,6 +75,31 @@ public class Problems {
         }
         return ans;
     }
+    static boolean isPossible(int[] a, int k, int distance){
+        int kidsPlaced = 1;
+        int lastKid = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if(a[i] - lastKid >= distance){
+                kidsPlaced++;
+                lastKid = a[i];
+            }
+        }
+        return kidsPlaced >= k;
+
+    }
+    static int raceTrack(int[] a, int k){
+        if( k > a.length) return -1;
+        int st = 0, end = (int)1e9;
+        int ans = -1;
+        while(st <= end){
+            int mid = st + (end - st)/2;
+            if(isPossible(a,k,mid)){
+                ans = mid;
+                st = mid+1;
+            } else end = mid-1;
+        }
+        return ans;
+    }
 
     static void main(String[] args) {
         int[] arr = {5,6,7,8,9,10,1,2,3,4};
